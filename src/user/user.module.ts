a9faa147358edc75a5aec943ccc4f885ea4e20af
Coenraad Fick <coenraad@responsive.co.za>
2019-06-13 11:36:02 +0200

    Added GraphQL Queries to all entities, including the links between them

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,10 +4,13 @@ import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { UserResolver } from './user.resolver';
+import { IdeaEntity } from '../idea/idea.entity';
+import { CommentEntity } from '../comment/comment.entity';
+import { CommentService } from '../comment/comment.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, IdeaEntity, CommentEntity])],
   controllers: [UserController],
-  providers: [UserService, UserResolver],
+  providers: [UserService, UserResolver, CommentService],
 })
 export class UserModule {}
